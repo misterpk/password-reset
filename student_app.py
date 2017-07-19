b2a4a3eb1b9b25125ba10555faf3c615b6c7b7dc
Nick Kim <nick.kim@logmein.com>
2017-07-19 10:28:11 -0700

    changes to support csv

--- student_app.py
+++ student_app.py
@@ -33,36 +33,47 @@ def draw_student_menu():
     return selection
 
 
-def add_student(file):
-    name = input("Enter the student's name: ")
-    nickname = input("Enter the student's nick name: ")
-    student_id = input("Enter the student's ID: ")
+def add_student():
+    with open('class_roster.txt', 'a') as class_roster:
+        name = input("Enter the student's name: ")
+        nickname = input("Enter the student's nick name: ")
+        student_id = input("Enter the student's ID: ")
 
-    student = Student(name, nickname, student_id)
+        student = Student(name, nickname, student_id)
 
-    student_json = json.dumps({student.student_id:
-                              [student.name,
-                               student.nickname,
-                               student.password]})
+        class_roster.write("{}, {}, {}, {}\n".format(student.student_id,
+                                                     student.name,
+                                                     student.nickname,
+                                                     student.password))
 
-    json.dump(student_json, file)
+        class_roster.close()
+
+
+def change_password():
+    with open('class_roster.txt', 'a') as class_roster:
+        new_password = input("The password must meet the following criteria:\n"
+                             "* At least 8 characters long\n* At least 1 "
+                             "lowercase letter\n* At least 1 uppercase "
+                             "letter.\n\nEnter a new password: ")
+
 
 if __name__ == "__main__":
-    with open('class_roster.txt', 'r+') as class_roster:
+    # with open('class_roster.txt', 'r+') as class_roster:
 
-        while True:
-            choice = draw_main_menu()
+    while True:
+        choice = draw_main_menu()
 
+        if choice == "01":
+            choice = draw_teacher_menu()
             if choice == "01":
-                choice = draw_teacher_menu()
-                if choice == "01":
-                    add_student(class_roster)
-                    class_roster.close()
-            elif choice == "02":
-                draw_student_menu()
-            elif choice.upper() == "Q":
-                exit(1)
-            else:
-                print("Invalid entry. Enter 01, 02 or Q.")
-                continue
+                add_student()
+        elif choice == "02":
+            choice = draw_student_menu()
+            if choice == "01":
+
+        elif choice.upper() == "Q":
+            exit(1)
+        else:
+            print("Invalid entry. Enter 01, 02 or Q.")
+            continue
 
